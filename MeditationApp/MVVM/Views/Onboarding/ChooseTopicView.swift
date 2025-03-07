2b513f47660b08324f8d283e79e99c959481f70e
Chetan Hedamba <chetan@7span.com>
2025-03-07 12:24:01 +0530

    Feat : Basic MVVM Structure created

---
+++ MeditationApp/MVVM/Views/Onboarding/ChooseTopicView.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseTopicView.swift
+//  MeditationApp
+//
+//  Created by Chetan Hedamba on 07/03/25.
+//
+
+import SwiftUI
+
+struct ChooseTopicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChooseTopicView()
+}
