c4050416a7cb5b4f0ef6c4e3c066c5f2ee7e7300
Chetan Hedamba <chetan@7span.com>
2025-03-07 12:09:37 +0530

    Initial Commit

---
+++ MeditationApp/MeditationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationAppApp.swift
+//  MeditationApp
+//
+//  Created by Chetan Hedamba on 07/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MeditationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
